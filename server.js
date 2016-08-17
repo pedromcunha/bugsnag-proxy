01b220ad8c1621205df67697354fe1b26b33afdf
Pedro <pedro@younow.com>
2016-08-17 18:52:45 -0400

    set to metaData from extraData

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.post("/", upload.any(), function (request, response) {
   if(file) {
     minidump.walkStack(file.path, function(error, report) {
       const data = {
-        extraData: {
+        metaData: {
           electron_version: request.body.ver,
           platform: request.body.platform
         }
